02e122fb87f476b86421fbb0bd18364ad565d0bd
efimov-top-academy <efimov_m@t.top-academy.ru>
2022-12-17 18:02:37 +0300

    Добавьте файлы проекта.

---
+++ WpfControlsContainersApp/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfControlsContainersApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Expander_Expanded(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Expander Expanded!");
+        }
+
+        private void Expander_Collapsed(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Expander Collapsed!");
+        }
+
+        private void Button_MouseEnter(object sender, MouseEventArgs e)
+        {
+            pop.IsOpen = true;
+        }
+    }
+}
